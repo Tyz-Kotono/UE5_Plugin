987c5fb11c21b92b9dcf6e0e7181104ac0f591ba
kotono <2713421980@qq.com>
2025-08-04 18:36:51 +0800

    整理Main Branch 文件

--- Source/LearnShader/ComputerShader/CSBlurShader.cpp
+++
@@ -1,2 +0,0 @@
-﻿#include "CSBlurShader.h"
-IMPLEMENT_SHADER_TYPE(, FCSBlurShader, TEXT("/LearnShader/ComputerShader/csShader.usf"), TEXT("MainCS"), SF_Compute);
--- Source/LearnShader/RenderTarget/RenderTargetShader.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "RenderTargetShader.h"
-IMPLEMENT_SHADER_TYPE(, FRenderTargetNewVS, TEXT("/LearnShader/Private/RenderTargetShader.usf"), TEXT("MainVertexS"), SF_Vertex);
-IMPLEMENT_SHADER_TYPE(, FRenderTargetNewPS, TEXT("/LearnShader/Private/RenderTargetShader.usf"), TEXT("MainPixelS"), SF_Pixel);
-
-IMPLEMENT_SHADER_TYPE(, FRenderTargetPS, TEXT("/LearnShader/Private/RenderTargetShader.usf"), TEXT("MainPS"), SF_Pixel);
